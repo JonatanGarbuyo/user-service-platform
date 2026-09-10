5478e6c06771beef0bf13f2687e6f5689d8350a8
Jonatan Garbuyo <58077441+JonatanGarbuyo@users.noreply.github.com>
2026-09-09 23:18:05 -0300

    chore: add eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig(
+  {
+    ignores: ['coverage/**', 'dist/**', '.wrangler/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strict,
+      tseslint.configs.stylistic,
+    ],
+    rules: {
+      eqeqeq: ['error', 'always'],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+)
